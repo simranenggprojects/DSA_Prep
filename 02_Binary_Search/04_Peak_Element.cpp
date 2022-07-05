17e054215167100421a467be6e952c64d1176004
Simran <66124450+simranenggprojects@users.noreply.github.com>
2022-07-05 16:41:40 +0530

    Add files via upload

---
+++ 02_Binary_Search/04_Peak_Element.cpp
@@ -0,0 +1,32 @@
+/*
+Problem Link
+https://practice.geeksforgeeks.org/problems/peak-element/1
+*/
+
+class Solution
+{
+    public:
+    int peakElement(int arr[], int n)
+    {
+       int l=0,h=n-1,mid;
+       
+       while(l<=h){
+           
+           mid = l + (h-l)/2;
+           
+           if ((mid == 0 || arr[mid-1] <= arr[mid]) && (mid == n-1 || arr[mid+1] <= arr[mid])){
+               return mid;
+           }
+           else if(mid > 0 && (arr[mid-1] > arr[mid])){
+               h = mid - 1;
+           }
+           else{
+               l = mid + 1;
+           }
+           
+       }
+       
+       return mid;
+    }
+    
+};
